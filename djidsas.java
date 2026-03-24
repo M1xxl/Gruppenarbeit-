f9812a42d200706955671032906604b09d9be035
Max Jordan <poesdami@gmail.com>
2026-03-24 11:43:05 +0100

    Klasse Studierende

--- djidsas.java
+++ djidsas.java
@@ -1,3 +1,43 @@
-public class djidsas {
+import java.util.Scanner;
+
+public class Studierende {
+
+
+public class  Studierender {
+    Scanner scanner = new Scanner (System.in);
+
+    private String name;
+    private int matrikelnummer;
+    private String studiengang;
+
+    public Studierender (String name, int matrikelnummer, String studiengang) {
+        this.name = name;
+        this.matrikelnummer = matrikelnummer;
+        this.studiengang = studiengang;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getMatrikelnummer() {
+        return matrikelnummer;
+    }
+
+    public String getStudiengang() {
+        return studiengang;
+    }
+
+    public void frageStellen (Dozierender dozierender) {
+        System.out.println("bitte gebe deine Frage an den Dozenten ein");
+        String frage = scanner.nextLine();
+        System.out.println("an welchen Dozenten soll die Frage gehen?");
+        String Dozentenname = scanner.nextLine();
+        dozierender.frageBeantworten(this, frage,Dozentenname);
+
+    }
+
+}
+
     
 }
