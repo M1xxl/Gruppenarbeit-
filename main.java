5bb3096d51a6f9339516143659bfb3d9319f0e3e
Max Jordan <poesdami@gmail.com>
2026-03-24 12:28:03 +0100

    Main

--- main.java
+++ main.java
@@ -1,3 +1,10 @@
+
 public class main {
-    
+    public static void main (String [] args) {
+
+        Studierender student = new Studierender("Thomas Gottschalk", 102939, "User Experience Design");
+        Dozierender dozent = new Dozierender("Keine Ahnung"," THI", 204 );
+
+        student.frageStellen(dozent);
+    }
 }
