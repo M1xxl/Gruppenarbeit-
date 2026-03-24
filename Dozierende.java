cd6d1cd4494a3fa36ac6ebd6802ce33efa45b083
Tobias Krauss <tobias.krauss@gmail.com>
2026-03-24 12:25:06 +0100

    dozierende

---
+++ Dozierende.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Dozierender {
+    Scanner scanner = new Scanner (System.in);
+
+    private String name;
+    private String fakultaet;
+    private int bueronummer;
+
+    public Dozierender (String name, String fakultaet, int bueronummer) {
+        this.name = name;
+        this.fakultaet = fakultaet;
+        this.bueronummer = bueronummer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void frageBeantworten(Studierender studierender, String frage, String Dozentenname) {
+        System.out.println("Frage von " + studierender.getName() + ": " + frage);
+        System.out.println("Die Frage geht an den Dozenten: " + Dozentenname);
+        System.out.println("Der Dozierende " + Dozentenname + " antwortet dem Studenten: " + studierender.getName());
+        String antwort = scanner.nextLine();
+        System.out.println(antwort);
+    }
+}
